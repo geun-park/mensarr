098ffac3af6f8d985815327466fc2cd1bc8999fb
ryobertolissi <ryobertolissi@gmail.com>
2024-10-12 10:20:27 +0200

    remove try catch

--- backend/app.py
+++ backend/app.py
@@ -7,43 +7,31 @@ app = Flask(__name__)
 CORS(app)
 
 # Connect to the PostgreSQL database using the DATABASE_URL environment variable
-try:
-    conn = psycopg2.connect(os.getenv("DATABASE_URL"))
-    cursor = conn.cursor()
-except Exception as e:
-    print(f"Error connecting to the database: {e}")
+conn = psycopg2.connect(os.getenv("DATABASE_URL"))
+cursor = conn.cursor()
+
 
 @app.route("/api/todos", methods=["GET"])
 def fetch_todos():
-    try:
-        cursor.execute("SELECT * FROM todos")
-        todos = cursor.fetchall()
-        return todos
-    except Exception as e:
-        print(f"Error fetching todos: {e}")
-        return jsonify({"error": "Error fetching todos"}), 500
+    cursor.execute("SELECT * FROM todos")
+    todos = cursor.fetchall()
+    return todos
+
 
 @app.route("/api/todos", methods=["POST"])
 def create_todo():
-    try:
-        content = request.json
-        cursor.execute("INSERT INTO todos (title) VALUES (%s) RETURNING id", (content["title"],))
-        new_id = cursor.fetchone()[0]
-        conn.commit()
-        return jsonify({"id": new_id, "title": content["title"]})
-    except Exception as e:
-        print(f"Error creating todo: {e}")
-        return jsonify({"error": "Error creating todo"}), 500
+    content = request.json
+    cursor.execute("INSERT INTO todos (title) VALUES (%s) RETURNING id", (content["title"],))
+    new_id = cursor.fetchone()[0]
+    conn.commit()
+    return jsonify({"id": new_id, "title": content["title"]})
+
 
 @app.route("/api/todos/<int:id>", methods=["DELETE"])
 def delete_todo(id):
-    try:
-        cursor.execute("DELETE FROM todos WHERE id = %s", (id,))
-        conn.commit()
-        return "OK"
-    except Exception as e:
-        print(f"Error deleting todo: {e}")
-        return jsonify({"error": "Error deleting todo"}), 500
+    cursor.execute("DELETE FROM todos WHERE id = %s", (id,))
+    conn.commit()
+    return "OK"
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
