e2e3a4b9fca7aafd6e0bf9efb1ee595ae78fa075
roliveir <roberto.oliveirapais@inf.ethz.ch>
2024-10-12 17:48:32 +0200

    merged geun

--- react-native-frontend/components/mensapage/Polymensa.tsx
+++ react-native-frontend/components/mensapage/Polymensa.tsx
@@ -3,20 +3,18 @@ import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import MapPolyterrase from '@/components/MapPolyterrase';
 import SideButton from '@/components/SideButton'
 
-const App = () => (
-  <GestureHandlerRootView style={{flex: 1}}>
 
-  </GestureHandlerRootView>
-);
 
 export default function Polymensa() {
   return (
+    <GestureHandlerRootView style={{flex: 1}}>
     <View style={{flex:1}}>
       <MapPolyterrase/>
       <SideButton>
       </SideButton>
 
     </View>
+    </GestureHandlerRootView>
     
   );
 }
