03f1f174e6df18051659b4b956a16cddd8bc275c
ryobertolissi <ryobertolissi@gmail.com>
2024-10-12 12:33:30 +0200

    conditional population

--- backend/init_db.py
+++ backend/init_db.py
@@ -5,74 +5,86 @@ import os
 conn = psycopg2.connect(os.getenv("DATABASE_URL"))
 cursor = conn.cursor()
 
-
-
-# Create the groups table if it doesn't exist
+#check if groups table exists
 cursor.execute("""
-CREATE TABLE groups (
-    id SERIAL PRIMARY KEY,
-    title VARCHAR(255) NOT NULL
+SELECT EXISTS (
+    SELECT FROM information_schema.tables 
+    WHERE table_schema = 'public' 
+    AND table_name = 'groups'
 )
 """)
+exists = cursor.fetchone()[0]
 
-# Insert initial groups data
-groups = [
-    (0, "test 1"),
-    (1, "test 2"),
-]
+if not exists:
 
-cursor.executemany("INSERT INTO groups (id, title) VALUES (%s, %s) ON CONFLICT DO NOTHING", groups)
 
-cursor.execute("""
-CREATE TABLE names (
-    id SERIAL NOT NULL PRIMARY KEY,
-    title  VARCHAR(255) NOT NULL UNIQUE
-)
-""")
 
-cursor.execute("""
-CREATE TABLE names_groups (
-    name_id INT REFERENCES names(id) On DELETE CASCADE On UPDATE CASCADE,
-    group_id INT REFERENCES groups(id) On DELETE CASCADE On UPDATE CASCADE,
-    PRIMARY KEY (name_id, group_id)
-)
-""")
+    # Create the groups table if it doesn't exist
+    cursor.execute("""
+    CREATE TABLE groups (
+        id SERIAL PRIMARY KEY,
+        title VARCHAR(255) NOT NULL
+    )
+    """)
 
-cursor.executemany("INSERT INTO names (id, title) VALUES (%s, %s) ON CONFLICT DO NOTHING", [
-    (0, "ryo"),
-    (1, "jonas"),
-])
+    # Insert initial groups data
+    groups = [
+        (0, "test 1"),
+        (1, "test 2"),
+    ]
 
-cursor.executemany("INSERT INTO names_groups (name_id, group_id) VALUES (%s, %s) ON CONFLICT DO NOTHING", [
-    (0, 0),
-    (1, 1),
-    (0, 1),
-])
+    cursor.executemany("INSERT INTO groups (id, title) VALUES (%s, %s) ON CONFLICT DO NOTHING", groups)
 
-cursor.execute("""
-CREATE TABLE mensa (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL
-)
-""")
-cursor.executemany("INSERT INTO mensa (id, name) VALUES (%s, %s) ON CONFLICT DO NOTHING", [
-    (0, "POLY"),
-])
+    cursor.execute("""
+    CREATE TABLE names (
+        id SERIAL NOT NULL PRIMARY KEY,
+        title  VARCHAR(255) NOT NULL UNIQUE
+    )
+    """)
 
-cursor.execute("""
-CREATE TABLE locations (
-    gid INT REFERENCES groups(id) ON DELETE CASCADE ON UPDATE CASCADE, 
-    location_x DOUBLE PRECISION NOT NULL,
-    location_y DOUBLE PRECISION NOT NULL,
-    joinable BOOLEAN DEFAULT FALSE,
-    mensa_id INT REFERENCES mensa(id) ON DELETE CASCADE ON UPDATE CASCADE,
-    PRIMARY KEY (gid) 
-)
-""")
+    cursor.execute("""
+    CREATE TABLE names_groups (
+        name_id INT REFERENCES names(id) On DELETE CASCADE On UPDATE CASCADE,
+        group_id INT REFERENCES groups(id) On DELETE CASCADE On UPDATE CASCADE,
+        PRIMARY KEY (name_id, group_id)
+    )
+    """)
 
-cursor.executemany("INSERT INTO locations (gid, location_x, location_y, mensa_id) VALUES (%s, %s, %s, %s) ON CONFLICT DO NOTHING", [
-    (0, 1.0, 1.0, 0),
-])
+    cursor.executemany("INSERT INTO names (id, title) VALUES (%s, %s) ON CONFLICT DO NOTHING", [
+        (0, "ryo"),
+        (1, "jonas"),
+    ])
+
+    cursor.executemany("INSERT INTO names_groups (name_id, group_id) VALUES (%s, %s) ON CONFLICT DO NOTHING", [
+        (0, 0),
+        (1, 1),
+        (0, 1),
+    ])
+
+    cursor.execute("""
+    CREATE TABLE mensa (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(255) NOT NULL
+    )
+    """)
+    cursor.executemany("INSERT INTO mensa (id, name) VALUES (%s, %s) ON CONFLICT DO NOTHING", [
+        (0, "POLY"),
+    ])
+
+    cursor.execute("""
+    CREATE TABLE locations (
+        gid INT REFERENCES groups(id) ON DELETE CASCADE ON UPDATE CASCADE, 
+        location_x DOUBLE PRECISION NOT NULL,
+        location_y DOUBLE PRECISION NOT NULL,
+        joinable BOOLEAN DEFAULT FALSE,
+        mensa_id INT REFERENCES mensa(id) ON DELETE CASCADE ON UPDATE CASCADE,
+        PRIMARY KEY (gid) 
+    )
+    """)
+
+    cursor.executemany("INSERT INTO locations (gid, location_x, location_y, mensa_id) VALUES (%s, %s, %s, %s) ON CONFLICT DO NOTHING", [
+        (0, 1.0, 1.0, 0),
+    ])
 
 
 
