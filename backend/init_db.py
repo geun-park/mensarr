acd06977b95559379fc8c9b22774ec7ca8eb7405
Jonas <jonahe@student.ethz.ch>
2024-10-12 12:04:31 +0200

    "init first draft"

--- backend/init_db.py
+++ backend/init_db.py
@@ -5,35 +5,75 @@ import os
 conn = psycopg2.connect(os.getenv("DATABASE_URL"))
 cursor = conn.cursor()
 
-# Check if the todos table exists
+
+
+# Create the groups table if it doesn't exist
 cursor.execute("""
-SELECT EXISTS (
-    SELECT FROM information_schema.tables 
-    WHERE table_name = 'todos'
-);
+CREATE TABLE groups (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL
+)
 """)
-table_exists = cursor.fetchone()[0]
 
-if not table_exists:
-    # Create the todos table if it doesn't exist
-    cursor.execute("""
-    CREATE TABLE todos (
-        id SERIAL PRIMARY KEY,
-        title VARCHAR(255) NOT NULL
-    )
-    """)
+# Insert initial groups data
+groups = [
+    (1, "test 1"),
+    (2, "test 2"),
+]
 
-    # Insert initial todos data
-    todos = [
-        (1, "Find a great team"),
-        (2, "Choose a project"),
-        (3, "Interview people for need-finding"),
-        (4, "Come up with a lo-fi prototype")
-    ]
+cursor.executemany("INSERT INTO groups (id, title) VALUES (%s, %s) ON CONFLICT DO NOTHING", groups)
 
-    cursor.executemany("INSERT INTO todos (id, title) VALUES (%s, %s) ON CONFLICT DO NOTHING", todos)
+cursor.execute("""
+CREATE TABLE names (
+    id SERIAL NOT NULL PRIMARY KEY,
+    title unique VARCHAR(255) NOT NULL
+)
+""")
 
-    conn.commit()
+cursor.execute("""
+CREATE TABLE names_groups (
+    name_id INT REFERENCES names(id) On DELETE CASCADE On UPDATE CASCADE,
+    group_id INT REFERENCES groups(id) On DELETE CASCADE On UPDATE CASCADE,
+    PRIMARY KEY (name_id, group_id)
+)
+""")
+
+cursor.executemany("INSERT INTO names (id, title) VALUES (%s, %s) ON CONFLICT DO NOTHING", [
+    (1, "ryo"),
+    (2, "jonas"),
+])
+
+cursor.executemany("INSERT INTO names_groups (name_id, group_id) VALUES (%s, %s) ON CONFLICT DO NOTHING", [
+    (1, 1),
+    (2, 2),
+    (1, 2),
+])
+
+cursor.execute("""
+CREATE TABLE mensa (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+)
+""")
+
+cursor.execute("""
+CREATE TABLE locations (
+    gid INT REFERENCES groups(id) ON DELETE CASCADE ON UPDATE CASCADE, 
+    location_x DOUBLE NOT NULL,
+    location_y DOUBLE NOT NULL,
+    joinable BOOLEAN DEFAULT FALSE,
+    mensa_id INT REFERENCES mensa(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    PRIMARY KEY (gid) 
+)
+""")
+
+cursor.executemany("INSERT INTO locations (gid, location_x, location_y, mensa_id) VALUES (%s, %s, %s, %s) ON CONFLICT DO NOTHING"), [
+    (0, 1.0, 1.0, 0),
+]
+
+
+
+conn.commit()
 
 cursor.close()
 conn.close()
